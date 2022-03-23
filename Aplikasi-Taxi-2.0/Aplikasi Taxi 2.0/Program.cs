0af57efc6c3fe17e625574f99f45124694d0a922
irvanshandika <shandikamuhammadirvan@gmail.com>
2022-03-23 14:37:13 +0700

    Aplikasi-Taxi-2.0

---
+++ Aplikasi-Taxi-2.0/Aplikasi Taxi 2.0/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace AplikasiTaxi
+{
+    public class Taxi
+    {
+        public string Nama { get; set; }
+        public string Lokasiawal { get; set; }
+        public string Tujuan { get; set; }
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+
+            Console.WriteLine("Lokasi Anda Sekarang : {0}", Lokasiawal);
+
+            Console.WriteLine("Tujuan Anda : {0}", Tujuan);
+        }
+    }
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Taxi myObj = new Taxi();
+            Console.Write("Masukkan Nama : ");
+            myObj.Nama = Console.ReadLine();
+
+            Console.Write("Lokasi Anda Sekarang : ");
+            myObj.Lokasiawal = Console.ReadLine();
+
+            Console.Write("Tujuan Anda : ");
+            myObj.Tujuan = Console.ReadLine();
+
+            //Output panggil method registrasi
+            myObj.Registrasi();
+        }
+    }
+}
