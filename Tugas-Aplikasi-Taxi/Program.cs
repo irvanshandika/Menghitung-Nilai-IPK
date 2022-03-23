a43a38accd565fdd37791d6f75f782c281cc5481
irvanshandika <shandikamuhammadirvan@gmail.com>
2022-03-23 22:58:05 +0700

    Tugas-Aplikasi-Taxi

---
+++ Tugas-Aplikasi-Taxi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pertama membuat objek terlebih dahulu, yaitu Taxi
+            Taxi taxi = new Taxi();
+
+            // menentukan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //membuat method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DroppOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
